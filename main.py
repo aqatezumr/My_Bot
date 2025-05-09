9e9f03040494ac5044246e8cca702246518ed834
aqatezumr <144799456+aqatezumr@users.noreply.github.com>
2025-05-09 18:59:20 +0300

    Update main.py
    add a "YOUR_BOT_TOKEN"

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I'm your Render-hosted bot!")
 
-app = ApplicationBuilder().token("YOUR_BOT_TOKEN").build()
+app = ApplicationBuilder().token("7032229248:AAGsgYzua_Wng2-oFh1pIn0qOhhXIhPgDHU").build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
